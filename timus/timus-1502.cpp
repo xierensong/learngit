0e57e233101c7f7261764eb06d1e08c493927985
xierensong <283318608@qq.com>
2018-03-11 08:38:34 +0800

    add 1502 result

---
+++ timus/timus-1502.cpp
@@ -0,0 +1,22 @@
+﻿// timus.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	long long n;
+	cin >> n;
+
+	cout << (n + 2)* (n + 1) * n / 2 << endl;
+
+    return 0;
+}
+
