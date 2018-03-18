ccf05def3a69a2dd8e14e80f77f67985026a24ba
xierensong <283318608@qq.com>
2018-03-18 10:49:36 +0800

    add 1035 result

---
+++ timus/timus-2035.cpp
@@ -0,0 +1,38 @@
+﻿// timus.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+	long X,Y,C;
+	long i;
+
+
+	cin >> X >> Y >> C;
+	if (X + Y < C)
+		cout << "Impossible" << endl;
+	else
+	{
+		for (i = 0; i <= X; i++)
+		{
+			if (C - i <= Y && C - i >= 0)
+			{
+				cout << i << ' ' << C - i << endl;
+				break;
+			}
+		}
+	}
+	
+
+
+    return 0;
+}
+
