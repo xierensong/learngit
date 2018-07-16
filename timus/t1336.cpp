3fc93acccd795bac9fad639a13e973a92ad118d9
unknown <283318608@qq.com>
2018-07-16 10:01:54 +0800

    add 1336 result

---
+++ timus/t1336.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        unsigned long long n = 0;
+
+        cin >> n;
+        cout << n * n << endl;
+        cout << n << endl;
+        return 0;
+}
