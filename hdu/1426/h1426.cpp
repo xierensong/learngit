15dc8e611d91f181abc5fb8a6502bed95b75a27e
xierensong <xierensong@github.com>
2018-11-07 13:27:43 +0800

    add 1426 result

--- hdu/1426/h1426.cpp
+++ hdu/1426/h1426.cpp
@@ -12,29 +12,51 @@ typedef struct POS
 } POS;
 
 int array[9][9];
-
+/*
+int array[9][9] = {{7,1,2,0,6,0,3,5,8},
+                   {0,6,5,2,0,7,1,0,4},
+                   {0,0,8,5,1,3,6,7,2},
+                   {9,2,4,0,5,6,0,3,7},
+                   {5,0,6,0,0,0,2,4,1},
+                   {1,0,3,7,2,0,9,0,5},
+                   {0,0,1,9,7,5,4,8,6},
+                   {6,0,7,8,3,0,5,1,9},
+                   {8,5,9,0,4,0,0,2,3}};
+*/
 vector<int> calc_num(int col)
 {
   vector<int> numList;
   int i = 0;
   int a[10];
+  //初始化a数组
   for(int i = 0; i < 9; i ++)
     a[i] = 0;
+  //已有的数字设置为1
   for(int i = 0; i < 9; i ++)
   {
     int index = array[i][col];
-    a[index] = 1;
+    if (index != 0)
+        a[index - 1] = 1;
   }
+/*
+  for (int i = 0; i < 9; i ++)
+  {
+    cout << i << '\t' << a[i] << endl;
+  }
+*/
+  //未设置为1的下标是候选数字
   for(int i = 0; i < 9; i ++)
   {
     if (a[i] == 0)
-        numList.push_back(i);
+        numList.push_back(i + 1);
   }
-  cout << "num list:" << endl;
+  //cout << "num list:" << endl;
+  /*
   for(vector<int>::iterator it = numList.begin();
         it < numList.end(); it++)
     cout << (*it) << '\t';
   cout << endl;
+  */
   return numList;
 }
 
@@ -52,10 +74,12 @@ vector<POS>  calc_pos(int col)
         pos_item.y = col;
         posList.push_back(pos_item);
     }
-  cout << "pos list:" << endl;
+  //cout << "pos list:" << endl;
+  /*
   for (vector<POS>::iterator it = posList.begin();
         it < posList.end(); it++)
     cout << it -> x << '\t' << it -> y << endl;
+  */
   return posList;
 }
 
@@ -64,10 +88,10 @@ void print()
   int i,j;
   for (i = 0; i < 9; i ++) 
   {   
-     cout << endl;
+     //cout << endl;
      for (j = 0; j < 9; j ++) 
      {   
-        cout << array[i][j] << '\t';
+        cout << array[i][j] << ' ';
      }   
      cout << endl;
   }
@@ -78,15 +102,86 @@ int check(int x, int y)
   int value = 0;
   int result = 0;
   value = array[x][y];
+  //cout << "enter check" << endl;
+  //cout << x << '\t' << y << '\t' << value << endl;
   //检查行是否有重复
-  for(int j = 0; j < 9 && j != y; j ++)
+  //cout << "== check row==" << endl;
+  for(int j = 0; j < 9; j ++)
   {
-    if (array[x][j] == value)
+    if (array[x][j] == value && j != y)
       return 0;
   }
   //检查局部是否有重复
-  
-  return 0;
+  //(0,0)
+  //cout << "check local" << endl;
+  if ((x >= 0) && (x <= 2) && (y >= 0) && (y <= 2)) 
+  {
+    for(int i = 0; i <= 2 && i != x; i++)
+      for(int j = 0; j <= 2 && j != y; j++)
+      {
+        if (array[i][j] == value)
+          return 0;
+      }
+  } 
+  //(0,1)
+  if (x >= 0 && x <= 2 && y >= 3 && y <= 5)
+  {
+    for (int i = 0; i <= 2 && i != x; i++)
+      for(int j = 3; j <= 5 && j != y; j++)
+      {
+        if (array[i][j] == value) return 0;
+      }
+  }
+  //(0,2)
+  if (x >= 0 && x <= 2 && y >= 6 && y <= 8)
+  {
+    for (int i = 0; i <= 2 && i != x; i++)
+      for(int j = 6; j <= 8 && j != y; j++)
+        if (array[i][j] == value) return 0;
+  }
+  //(1,0)
+  if (x >= 3 && x <= 5 && y >= 0 && y <= 2)
+  {
+    for (int i = 3; i <= 5 && i != x; i++)
+      for(int j = 0; j <= 2 && j != y; j++)
+        if (array[i][j] == value) return 0;
+  }
+  //(1,1)
+  if (x >= 3 && x <= 5 && y >= 3 && y <= 5)
+  {
+    for (int i = 3; i <= 5 && i != x; i++)
+      for(int j = 3; j <= 5 && j != y; j++)
+        if (array[i][j] == value) return 0;
+  }
+  //(1,2)
+  if (x >= 3 && x <= 5 && y >= 6 && y <= 8)
+  {
+    for (int i = 3; i <= 5 && i != x; i++)
+      for(int j = 6; j <= 8 && j != y; j++)
+        if (array[i][j] == value) return 0;
+  }
+  //(2,0)
+  if (x >= 6 && x <= 8 && y >= 0 && y <= 2)
+  {
+    for (int i = 6; i <= 8 && i != x; i++)
+      for(int j = 0; j <= 2 && j != y; j++)
+        if (array[i][j] == value) return 0;
+  }
+  //(2,1)
+  if (x >= 6 && x <= 8 && y >= 3 && y <= 5)
+  {
+    for (int i = 6; i <= 8 && i != x; i++)
+      for(int j = 3; j <= 5 && j != y; j++)
+        if (array[i][j] == value) return 0;
+  }
+  //(2,2)
+  if (x >= 6 && x <= 8 && y >= 6 && y <= 8)
+  {
+    for (int i = 6; i <= 8 && i != x; i++)
+      for(int j = 6; j <= 8 && j != y; j++)
+        if (array[i][j] == value) return 0;
+  }
+  return 1;
 }
 
 int dfs()
@@ -94,11 +189,12 @@ int dfs()
   vector<POS> posList;
   vector<int> numList;
   int lenNum, lenPos;  
-  cout << "start dfs" << endl;
+  //cout << "start dfs" << endl;
+  //print();
   // 遍历每一列
   for(int i = 0; i < 9; i++)
   {
-    cout << "colomn number: " << i << endl;
+    //cout << "colomn number: " << i << endl;
     numList = calc_num(i);
     posList = calc_pos(i);  
 
@@ -115,36 +211,46 @@ int dfs()
             it < posList.end(); it++)
     {
         int flag = 0;
+        //cout << "current postion" << endl;
+        int x = it -> x;
+        int y = it -> y;
         //遍历候选数字
         for (vector<int>::iterator num_it = numList.begin();
                num_it < numList.end(); num_it++)
         {
-            int x = it -> x;
-            int y = it -> y;
             int num = (*num_it);
+            /*
             cout << "x y array[x][y]" << '\t' << x
                 << '\t' << y << '\t' << num << endl;
-
+            */
             array[x][y] = num;
             // 如果dfs结果为1,说明当前候选数字可用
             if (check(x,y))
             {
-                cout << "" << endl;
-                //删除成功插入的候选位置信息
-                posList.erase(it);
-                //删除成功插入的候选数字信息
-                numList.erase(num_it);
-                //设置成功标志
-                flag = 1;
-                //不再遍历后面的数字
-                break;
+                //cout << "check success!" << endl;
+                //遍历后面的空位
+                if (dfs())
+                { 
+                  //设置成功标志
+                  flag = 1;
+                  //不再遍历后面的数字
+                  break;
+                }
+                else
+                {
+                  //cout << "left dfs fail,rollback!" << endl;
+                  array[x][y] = 0;
+                  //print(); 
+                   
+                }
             }
-            //dfs结果不为1，说明候选数字不可用
+            //check结果不为1，说明候选数字不可用
             else
             {
-                cout << "" << endl;
+                //cout << "check fail,rollback" << endl;
                 //回滚设置的值
                 array[x][y] = 0;
+                //print();
             }
         }
         //如果没有填入正确值
@@ -153,19 +259,21 @@ int dfs()
             //返回失败
             return 0;
         }
+        //print();
     } 
   }
-  cout << "end dfs" << endl;
+  //cout << "end dfs" << endl;
   return 1;
 }
 
 int main()
 {
-
+ 
   while(1)
   {
     char c, d;
     int i = 0, j = 0;
+    
     for (i = 0; i < 9; i ++)
     {
       for (j = 0; j < 9; j ++)
@@ -177,16 +285,19 @@ int main()
             array[i][j] = 0;
       }
     }
-    print();
+    
 
     if (dfs())
     {
-      cout << "成功" << endl;
+      print();
+      //cout << "成功" << endl;
     }
+    /*
     else
     {
       cout << "失败" << endl;
     }
+    */
   }
   return 0;
 }
